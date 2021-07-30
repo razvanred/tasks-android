b967cd6deac8df498feac6a7833dbda5117169a3
Răzvan Roșu <razvanred99@gmail.com>
2021-07-30 23:10:47 +0200

    Switch from plugins block to buildscript block for SediciTasks project Convert main build.gradle.kts project to Groovy Add Hilt Gradle Plugin dependency

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,30 +1,9 @@
-pluginManagement {
-    repositories {
-        gradlePluginPortal()
-        google()
-        mavenCentral()
-    }
-    plugins {
-        id("com.android.application") version "7.1.0-alpha05"
-        id("com.android.library") version "7.1.0-alpha05"
-        id("org.jetbrains.kotlin.android") version "1.5.10"
-        id("org.jetbrains.kotlin.jvm") version "1.5.10"
-        id("org.jetbrains.kotlin.kapt") version "1.5.10"
-    }
-}
-dependencyResolutionManagement {
-    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
-    repositories {
-        google()
-        mavenCentral()
-    }
-}
-
-enableFeaturePreview("VERSION_CATALOGS")
-enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
-
 rootProject.name = "SediciTasks-Android"
-include(":android-app")
+
 include(":model")
 include(":data:local:common")
 include(":data:local:android:common")
+include(":android-app")
+
+enableFeaturePreview("VERSION_CATALOGS")
+enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
