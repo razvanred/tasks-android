26817ef761178b564aacce6b6de0da5f00a07fc3
Răzvan Roșu <razvanred99@gmail.com>
2021-08-01 19:53:08 +0200

    Apply license to files through spotless

--- spotless/copyright.kt
+++ spotless/copyright.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2021 Răzvan Roșu
+ * Copyright $YEAR Răzvan Roșu
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,7 +14,3 @@
  * limitations under the License.
  */
 
-package app.sedici.tasks.model
-
-@JvmInline
-value class TaskId(val value: ULong)
